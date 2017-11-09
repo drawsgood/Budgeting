f07b431c813a05abc761535bc73c7c0dd984bca6
Michael Myers <mbmyers@gmail.com>
2017-11-09 10:53:20 -0600

    added css and edited index

--- index.php
+++ index.php
@@ -2,25 +2,29 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<link rel="stylesheet" type="text/css" href="/css/style.css">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 	<script src="My_Script.js"></script>
 	<title>My Testicles</title>
 </head>
 <body>
+	<div id="mainForm">
 	<form action="addExpense.php" method="POST">
-		<ul>
-		<li><label>Number</label>
+		<ul class="clearListStyling">
+		<li><label>Amount</label>
 		<input type="text" name="amount"/></li>
 		<li><label>Name of expense</label>
 		<input type="text" name="category"/></li>
-		<li><input type="submit" name="submit" value="Append"/></li>
+		<li><input type="submit" name="submit" value="Submit"/></li>
 		</ul>
 	</form>
 
 	<ul id="list"></ul>
 	Remaining budget for the day:
 	<ul id="total"></ul>
+
+	</div>
 
 </body>
 </html>
