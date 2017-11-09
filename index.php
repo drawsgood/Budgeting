b3ebf8956ae0e87b9e15070f3f3c0194c1d183e2
Michael Myers <mbmyers@gmail.com>
2017-11-09 10:27:42 -0600

    updated index.php

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 	<script src="My_Script.js"></script>
 	<title>My Testicles</title>
