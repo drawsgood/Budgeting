d888af73782fbc02d5a8d3fd01ea941384b4763a
Michael Myers <mbmyers@gmail.com>
2017-11-11 21:05:56 -0600

    changed index.php to index.html - moved My_Script.js to head

--- My_Script.js
+++ My_Script.js
@@ -1,23 +1,16 @@
 var finalNum = 50;
 $(document).ready(function() {
-setTimeout(function()
-{
 	$.getJSON("current.json", function(obj) {
-
 		var myVal = obj.startingValue;
-
 				$.each(obj.entries, function(key, value) {
 					finalNum -= Number(value.amount);
 					$('#list').append("<tr><td>$"+value.amount+"</td><td>"+value.category+"</td>");
 				});
 
-
-					$('#total').append("<h1 class='bg-primary text-center'>$"+myVal.fart.toFixed(2)+"</h1>");		
-					console.log(myVal.fart.toFixed(2));		
-
+				$('#total').append("<h1 class='bg-primary text-center'>$"+myVal.fart.toFixed(2)+"</h1>");		
+				console.log(myVal.fart.toFixed(2));		
 
 				console.log(finalNum.toFixed(2));
 				$('input[id=balance]').attr('value', finalNum.toFixed(2));
 	});
-}, 100);
 });
